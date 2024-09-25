193034c39926857103bf6354f82707a79cd22234
a.semenova <invisible2234@gmail.com>
2024-09-26 06:17:49 +0700

    - added axios to the project - created query fetch data users

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+export default {
+    getUsers() {
+        return apiClient.get('/users');
+    }
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from '@/services/api';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users: [],
+    error: null,
   },
+
   getters: {
+    users: state => state.users,
+    error: state => state.error,
   },
+
   mutations: {
+    SET_USERS(state, users) {
+      state.users = users;
+    },
+    SET_ERROR(state, error) {
+      state.error = error;
+    }
   },
+
   actions: {
+    async fetchUsers({ commit }) {
+      try {
+        const response = await api.getUsers();
+        commit('SET_USERS', response.data);
+      } catch (error) {
+        commit('SET_ERROR', error);
+        console.error('Ошибка при получении данных пользователей:', error);
+      }
+    }
   },
   modules: {
   }
