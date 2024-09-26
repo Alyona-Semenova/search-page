60b53990cd40c06f8637cea5ea867a86cf44862d
a.semenova <invisible2234@gmail.com>
2024-09-26 18:27:11 +0700

    - add loader

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="app">
+    <LoaderComponent  />
     <router-view/>
   </div>
 </template>
 
+<script>
+import LoaderComponent from "@/components/LoaderComponent.vue";
+import { mapGetters } from 'vuex';
+
+export default {
+  components: {
+    LoaderComponent,
+  },
+  computed: {
+    ...mapGetters(['isLoading'])
+  }
+}
+</script>
+
 <style lang="scss">
+// font
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap'); // font
 
 #app {
